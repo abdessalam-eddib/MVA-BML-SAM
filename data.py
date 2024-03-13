7c553e0a5d4bdecab00f3c97ed6a657cad3c3409
Fatima-ZahraeX <125985103+Fatima-ZahraeX@users.noreply.github.com>
2024-03-13 18:19:27 +0100

    Update data.py

--- data.py
+++ data.py
@@ -7,7 +7,10 @@ def dataloader(datasetname : str):
     """ returns dataloaders for a given dataset """
 
     datasets = { 'celebA': _celebA, 
-                 'stanfordcars': _stanfordcars}
+                 'stanfordcars': _stanfordcars,
+                 'mnist': _mnist,
+                'cifar10': _cifar10
+               }
 
     return datasets[datasetname.lower()]
 
@@ -21,6 +24,31 @@ def _celebA(
     ])
 
     trainset = torchvision.datasets.CelebA(
+        root=datasetfolder, train=True, 
+        download=True, transform=transform_totensor)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=batchsize, shuffle=True, 
+        num_workers=nworkers)
+
+    testset = torchvision.datasets.CelebA(
+        root=datasetfolder, train=False, 
+        download=True, transform=transform_totensor)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=testbatchsize, shuffle=False, 
+        num_workers=nworkers)
+
+    return trainset, testset, trainloader, testloader
+
+def _mnist(
+        batchsize : int, testbatchsize : int, datasetfolder : str, 
+        augment : bool = False, nworkers : int = 8):
+    """ MNIST, 60000 28x28x1 images, 10 classes, 10000 test images """
+
+    transform_totensor = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    trainset = torchvision.datasets.MNIST(
         root=datasetfolder, train=True, 
         download=True, transform=transform_totensor)
     trainloader = torch.utils.data.DataLoader(
@@ -45,7 +73,7 @@ def _stanfordcars(
         transforms.ToTensor(),
     ])
 
-    trainset = torchvision.datasets.FashionMNIST(
+    trainset = torchvision.datasets.StanfordCars(
         root=datasetfolder, train=True, 
         download=True, transform=transform_totensor)
     trainloader = torch.utils.data.DataLoader(

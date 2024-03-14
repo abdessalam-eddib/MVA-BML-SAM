ce416f056348f0fe1d53a067bec1360fbb158606
Abdessalam Ed-dib <88110981+abdessalam-eddib@users.noreply.github.com>
2024-03-14 23:16:30 +0100

    Fixing bugs

--- data.py
+++ data.py
@@ -8,8 +8,7 @@ def dataloader(datasetname : str):
 
     datasets = {'cifar10': _cifar10,
                 'food101': _food101,
-                'stanfordcars': _stanfordcars,
-                'fakedata': _fakedata,
+                'stl10': _stl10,
                }
 
     return datasets[datasetname.lower()]
@@ -109,7 +108,7 @@ def _food101(
     return trainset, testset, trainloader, testloader
 
 
-def _stanfordcars(
+def _stl10(
         batchsize: int, testbatchsize: int, datasetfolder: str,
         augment: bool = True, nworkers: int = 8):
     """ Stanford Cars, 196 makes, 8144 models, 16,185 images """
@@ -149,73 +148,6 @@ def _stanfordcars(
     testset = torchvision.datasets.STL10(
         root=datasetfolder, split="test", 
         download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=testbatchsize,
-        shuffle=False, num_workers=nworkers, drop_last=False)
-
-    return trainset, testset, trainloader, testloader
-
-
-def _fakedata(
-        batchsize: int, testbatchsize: int, datasetfolder: str,
-        augment: bool = True, nworkers: int = 8):
-    """ FakeData, randomly generated data for debugging """
-
-    trainset = torchvision.datasets.FakeData(
-        size=50000, image_size=(3, 224, 224), num_classes=10,
-        transform=transforms.ToTensor())
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=batchsize,
-        shuffle=True, num_workers=nworkers, drop_last=True)
-
-    testset = torchvision.datasets.FakeData(
-        size=10000, image_size=(3, 224, 224), num_classes=10,
-        transform=transforms.ToTensor())
-    testloader = torch.utils.data.DataLoader(
-        testset, batch_size=testbatchsize,
-        shuffle=False, num_workers=nworkers, drop_last=False)
-
-    return trainset, testset, trainloader, testloader
-
-
-def _caltech101(
-        batchsize: int, testbatchsize: int, datasetfolder: str,
-        augment: bool = True, nworkers: int = 8):
-    """ Caltech101, 101 object categories, 9144 images """
-
-    train_mean = (0.5, 0.5, 0.5)
-    train_std = (0.5, 0.5, 0.5)
-
-    if augment:
-        transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(train_mean, train_std),
-        ])
-    else:
-        transform_train = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(train_mean, train_std),
-        ])
-
-    transform_test = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(train_mean, train_std),
-    ])
-
-    trainset = torchvision.datasets.ImageFolder(
-        os.path.join(datasetfolder, 'caltech-101', 'train'), transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=batchsize,
-        shuffle=True, num_workers=nworkers, drop_last=True)
-
-    testset = torchvision.datasets.ImageFolder(
-        os.path.join(datasetfolder, 'caltech-101', 'test'), transform=transform_test)
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=testbatchsize,
         shuffle=False, num_workers=nworkers, drop_last=False)
